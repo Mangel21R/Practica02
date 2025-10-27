933e65557545ebcf4ae80c196320a0f47de63224
Angel Ramirez <rocjala22@gmail.com>
2025-10-27 13:03:49 -0500

    Corrige error de redondeo a 2 decimales (fixes #3)

--- conversor.py
+++ conversor.py
@@ -14,7 +14,7 @@ def convertir(precio_usd, moneda_destino, tasas):
     tasa = tasas["USD"].get(moneda_destino)
     if not tasa:
         raise ValueError("Moneda no soportada")
-    return precio_usd * tasa
+    return round(precio_usd * tasa, 2)
 
 
 def registrar_transaccion(producto, precio_convertido, moneda, ruta_log):
